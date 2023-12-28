8ac47cec98e400001997821574de40ec35d42cdd
Ruckstar <caraledso@gmail.com>
2023-12-28 14:31:28 -0600

    Modificiaciones Respecto a Paginación. Mejoras en botones de páginas y barra de búsqueda

--- store/views.py
+++ store/views.py
@@ -3,6 +3,8 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 # Create your views here.
 def store(request, category_slug=None):
     #PAra filtrar por categorias
@@ -11,16 +13,27 @@ def store(request, category_slug=None):
     
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category = categories, is_avaliable = True)
+        products = Product.objects.filter(category = categories, is_avaliable = True).order_by('id')
+        paginator = Paginator(products, 3)
+        #Capturando el valor de la página desde la url ()
+        page = request.GET.get('page')
+        #Indicando que envíe la lista de páginas (que envíe los productos que están en la página en cuestión)
+        paged_products = paginator.get_page(page)
         product_count = products.count()
         
     else:
-        products = Product.objects.all().filter(is_avaliable = True)
+        products = Product.objects.all().filter(is_avaliable = True).order_by('id')
+        #Para obtener pagincacion (Solo mostrar cierta cantidad de productos por página)
+        paginator = Paginator(products, 3)
+        #Capturando el valor de la página desde la url ()
+        page = request.GET.get('page')
+        #Indicando que envíe la lista de páginas (que envíe los productos que están en la página en cuestión)
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     
@@ -45,3 +58,20 @@ def product_detail(request, category_slug, product_slug):
     }
     
     return render(request, 'store/product_detail.html', context)
+
+#Método para búsqueda específica de un producto (Barra de búsqueda)
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        #Comparando la búsqueda ingresada por el usuario con los datos guardados edel producto (descripción o nombre del producto)
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count=products.count()
+            
+    #Diciconario para almacenar las variables consultadas
+    context = {
+        'products':products,
+        'product_count':product_count,
+    }
+    
+    return render(request, 'store/store.html', context)
