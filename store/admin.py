2756e73ec476e2fc61f867a712add4567c08da96
Ruckstar <caraledso@gmail.com>
2023-12-29 08:42:48 -0600

    Añadiendo Variations (variaciones en productos: tallas, colores, etc) Pt1

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_avaliable')
     prepopulated_fields = {'slug': ('product_name',)}
     
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value','is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value', 'is_active')
+    
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
+
+#Los modelos y clases admin.ModelAdmin son como se mostrarán idealmente en el administrador de Django
+
+#No olvidar realizar proceso de Migrationpara registrar en DB
+    #python manage.py makemigrations
+    #python manage.py migrate
 
--- store/models.py
+++ store/models.py
@@ -22,4 +22,40 @@ class Product(models.Model):
     #Método para obtener y mostrar en el home el nombre del producto
     def __str__(self):
         return self.product_name
+    
+    
+    
+#Clase para diferenciar entre variaciones (Diferenciar variaciones de tallas, colores, etc)
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager,self).filter(variation_category='color', is_active=True)
+    def tallas(self):
+        return super(VariationManager,self).filter(variation_category='talla', is_active=True)
+    
 
+#Diccionario para el elemento Choices de Clase Variation
+variation_category_choice=(
+    ('color', 'color'),
+    ('talla', 'talla'),
+)
+#Clase para las variaciones de productos(Tallas, colores, etc)
+class Variation(models.Model):
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+    
+    #Instanciando VariationManager para podr utilizar sus métodos
+    objects = VariationManager()
+    
+    #__str__ retorna unicamente el nombre del objeto, ya que se está transformando a string
+    # def __str__(self):
+    #     return str(self.product)
+    
+    #__unicode__ retorna el objeto de tipo objeto. Se debe realizar la respectiva variacion en admin.py para que muetsre todos los
+    #datos del objeto en cuestión y no solo un variationobject
+    def __unicode__(self):
+        return self.product
+    
+#No olvidar registar los modelso en Admin.py
